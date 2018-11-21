2339b356d7c8b62e9dc70ae607ca14cd7cc2a8c5
Gavin <deaboway@163.com>
2018-11-21 14:35:36 +0800

    删除不需要的内容，用migration代替sql导入数据。

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -9,17 +9,27 @@ class m130524_201442_init extends Migration
         $tableOptions = null;
         if ($this->db->driverName === 'mysql') {
             // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+            $tableOptions = 'CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE=MyISAM';
         }
 
-        $this->createTable('{{%user}}', [
+        $this->createTable('{{%menu}}', [
             'id' => $this->primaryKey(),
-            'username' => $this->string()->notNull()->unique(),
+            'name' => $this->string(128)->notNull(),
+            'parent' => $this->integer(),
+            'route' => $this->string(),
+            'order' => $this->integer(),
+            'icon' => $this->string(128),
+        ], $tableOptions);
+
+        $this->createTable('{{%admin}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(32)->notNull(),
+            'nickname' => $this->string(32)->notNull()->comment('用户昵称'),
+            'head_pic' => $this->string(255)->notNull()->comment('用户头像'),
             'auth_key' => $this->string(32)->notNull(),
             'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
-
+            'password_reset_token' => $this->string(),
+            'email' => $this->string()->notNull(),
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
@@ -28,6 +38,7 @@ class m130524_201442_init extends Migration
 
     public function down()
     {
-        $this->dropTable('{{%user}}');
+        $this->dropTable('{{%admin}}');
+        $this->dropTable('{{%menu}}');
     }
 }
